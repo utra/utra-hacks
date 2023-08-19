9b3c9d54abd43ac0a340f13962926130684cb827
AniqaT <aniqatahseen1@gmail.com>
2023-08-18 22:49:29 -0400

    Developed the home page for the website, added relevant home page related photos and edited navbar

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,26 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import utra_logo from "./logos/UTRA_logo_square.png";
-import './App.css';
+import RI_logo from "./Home Page Photos/RI.svg"
+import toronto_skyline from "./Home Page Photos/torontoSkyline2.png";
+import './Home.css';
 
 function Home() {
   return (
-    <h1>Welcome to UTRA Hacks!</h1>
+    <div className = "bg text text-center pt-5">   
+      <h1 className="pb-4">Welcome to UTRAHacks!</h1>
+      <img className = "pt-3 pb-5" src= {toronto_skyline} alt = "Toronto Syline"/>
+      <h2 className="pt-3">UTRAHacks 2023</h2>
+      <h5>November 4-5, 2023</h5>
+      <h5 className="pt-3">For sponsorship information, contact <a href="mailto:sponsorhacks@utra.ca" className='text'>sponsorhacks@utra.ca</a></h5>
+      <h5>Presented By:</h5>
+      <div>
+        <img style={{width: "5%"}} src= {utra_logo} alt = "UTRA"/>
+        <img style={{width: "25%"}} src= {RI_logo} alt = "University of Toronto & Robotics Institute"/>
+      </div>
+
+    </div> 
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -3,13 +3,14 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import {LinkContainer} from 'react-router-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import utra_logo from "./logos/utraLogoWhite.png";
 
 function NavBar() {
     return (
-        <Navbar bg = "dark" data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
+        <Navbar bg = "dark" data-bs-theme="dark" expand="lg" className="bg-body-tertiary" style = {{fontFamily: "Copperplate, Fantasy"}}>
             <Container>
                 <LinkContainer to = "/">
-                    <Navbar.Brand>UTRA Hacks</Navbar.Brand>  
+                    <Navbar.Brand><img src= {utra_logo} width = "160" alt = "UTRA Logo"/></Navbar.Brand>  
                 </LinkContainer>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
