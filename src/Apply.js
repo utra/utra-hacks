df679b7b82a948f6177b95d835054b6331a65758
AniqaT <aniqatahseen1@gmail.com>
2023-10-31 22:28:21 -0400

    Increased the apply section font size and changed the contact us submit button to a lighter colour

--- src/Apply.js
+++ src/Apply.js
@@ -4,7 +4,7 @@ function Apply() {
     return (
       <div id="apply" className="section">
         <h1 className="text-center">Apply Now</h1>
-        <p>Applications for hackers are now open!!</p>
+        <p style={{fontSize:"25px"}}>Applications for hackers are now open!!</p>
         <Button variant="light">Submit Applications Here</Button>{' '}
       </div>
     );
