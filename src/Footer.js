157f0208ec378410fb1286f1c43f5b4d7fc4983c
Richelle Pereira <richelle0001@gmail.com>
2023-11-07 21:46:54 -0500

    Removed the buttons in the footer of the website.

--- src/Footer.js
+++ src/Footer.js
@@ -5,14 +5,8 @@ export default function Footer() {
   return (
     <div className>
       <div className="bg-dark text-center text-white">
-        <div>
-            <Button></Button>
-            <Button></Button>
-            <Button></Button>
-            <Button></Button>
-        </div>
         <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-          © 2020 Copyright: All Rights Reserved
+          © 2023 University of Toronto Robotics Association
         </div>
       </div>
     </div>
