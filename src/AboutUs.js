47118c6f28ac763f20bfb74ef24f36b67c4cf820
AniqaT <aniqatahseen1@gmail.com>
2023-11-18 16:05:51 -0500

    Removed padding from about us section

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -7,7 +7,7 @@ function AboutUs() {
     <Container fluid id="about" className="section">
       <div>
         <h1>About Us</h1>
-        <p className="section about mx-3 px-3">
+        <p className="section about">
           At UTRA, we aim to inspire creativity, enable collaboration, and foster skill development. Participants can connect face-to-face, collaborate with global students, and engage with industry leaders and mentors. 
           The event sparks inspiration and opens minds to endless possibilities. Hackers of all experience levels can join workshops, tech talks, and career sessions to learn something new. UTRA Hacks supports participants by 
           providing necessary equipment, so that the sky's the limit!
