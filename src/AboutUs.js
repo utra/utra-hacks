98953f68737aba0e9c0ebee58026b5e7ba75e3cc
AniqaT <aniqatahseen1@gmail.com>
2023-11-18 16:04:42 -0500

    Fixed font size of about us section

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import './AboutUs.css';
+import { Container } from 'react-bootstrap';
 
 function AboutUs() {
   return (
-    <div id="about" className="section">
-      <div className="container" style={{paddingTop:"12%", paddingBottom:"10%"}}>
-        <h1 className="main-title title" >About Us</h1>
-        <p className="section about">
+    <Container fluid id="about" className="section">
+      <div>
+        <h1>About Us</h1>
+        <p className="section about mx-3 px-3">
           At UTRA, we aim to inspire creativity, enable collaboration, and foster skill development. Participants can connect face-to-face, collaborate with global students, and engage with industry leaders and mentors. 
           The event sparks inspiration and opens minds to endless possibilities. Hackers of all experience levels can join workshops, tech talks, and career sessions to learn something new. UTRA Hacks supports participants by 
           providing necessary equipment, so that the sky's the limit!
         </p>
       </div>
-    </div>
+    </Container>
   );
 }
 
